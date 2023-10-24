be69749ec4e901963463920d58c112cd4c0a1cad
ValentinaHotsa <134721626+ValentinaHotsa@users.noreply.github.com>
2023-10-24 17:35:47 +0200

    add info+styles

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,5 +1,13 @@
+import Notiflix from 'notiflix';
+import SlimSelect from 'slim-select';
+export { fetchBreeds, fetchCatByBreed };
 const selectList = document.querySelector('.breed-select');
+const loader = document.querySelector('.loader');
+const containerInfo = document.querySelector('.cat-info');
 function fetchBreeds() {
+  selectList.classList.add('is-hidden');
+  loader.classList.remove('is-hidden');
+
   return fetch('https://api.thecatapi.com/v1/breeds')
     .then(response => {
       if (!response.ok) {
@@ -10,11 +18,76 @@ function fetchBreeds() {
     .then(data => {
       const marcup = data
         .map(dataCat => {
-          return `<li><option value="${dataCat.id}">${dataCat.name}</option></li>
-        `;
+          return `<option value="${dataCat.id}">${dataCat.name}</option>`;
         })
         .join('');
       selectList.insertAdjacentHTML('afterbegin', marcup);
+      selectList.classList.remove('is-hidden');
+    })
+    .then(() => new SlimSelect({ select: `.breed-select` }))
+    .catch(() => {
+      Notiflix.Notify.failure(
+        'Oops! Something went wrong! Try reloading the page!'
+      );
+    })
+    .finally(() => {
+      loader.classList.add('is-hidden');
     });
 }
-export { fetchBreeds };
+function fetchCatByBreed(breedId) {
+  loader.classList.remove('is-hidden');
+  containerInfo.classList.add('is-hidden');
+  return fetch(
+    `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`
+  )
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+      const el = data.map(img => {
+        return `<div class='container-img'><img src="${img.url}" alt="${img.name}"></div>`;
+      });
+      containerInfo.innerHTML = el;
+      containerInfo.classList.remove('is-hidden');
+    })
+    .catch(() => {
+      Notiflix.Notify.failure(
+        'Oops! Something went wrong! Try reloading the page!'
+      );
+    })
+    .finally(() => {
+      loader.classList.add('is-hidden');
+      fetchCatInfo(breedId);
+    });
+}
+function fetchCatInfo(breedId) {
+  loader.classList.remove('is-hidden');
+
+  return fetch(`https://api.thecatapi.com/v1/breeds/${breedId}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(infoData => {
+      console.log(infoData);
+      const elem = `<div class='container-text'><h1 class='info-header'>${infoData.name}</h1>
+<p class='info-descr'>${infoData.description}</p>
+<p class='info-temp'>Temperament:${infoData.temperament}</p></div>`;
+
+      containerInfo.insertAdjacentHTML('beforeend', elem);
+    })
+    .catch(() => {
+      Notiflix.Notify.failure(
+        'Oops! Something went wrong! Try reloading the page!'
+      );
+    })
+    .finally(() => {
+      loader.classList.add('is-hidden');
+    });
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-import { fetchBreeds } from '../src/cat-api';
+import { fetchBreeds, fetchCatByBreed } from '../src/cat-api';
+import 'slim-select/dist/slimselect.css';
 
 axios.defaults.headers.common['x-api-key'] =
   'live_rOkkU9BWpeucDalppHqtUSRmiR6id6dH8l35GXaTbiCKFsAAhlfvBWnJ0wKNnHWt';
@@ -7,6 +8,13 @@ axios.defaults.headers.common['x-api-key'] =
 const selectList = document.querySelector('.breed-select');
 const loader = document.querySelector('.loader');
 const error = document.querySelector('.error');
-const containerInfo = document.querySelector('.cat-info');
+
+error.classList.add('is-hidden');
+loader.classList.add('is-hidden');
 
 fetchBreeds();
+selectList.addEventListener('change', changeSelect);
+function changeSelect(event) {
+  const selectValue = event.currentTarget.value;
+  fetchCatByBreed(selectValue);
+}
